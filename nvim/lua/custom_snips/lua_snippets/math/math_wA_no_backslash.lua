02a5482f65e40d7c2eeed22b02a25518029d1ef2
Chanhyuk Park <chan.hyuk.58@gmail.com>
2026-01-22 10:27:11 -0600

    2026-01-22 10:27 math snippets

--- nvim/lua/custom_snips/lua_snippets/math/math_wA_no_backslash.lua
+++ nvim/lua/custom_snips/lua_snippets/math/math_wA_no_backslash.lua
@@ -22,11 +22,12 @@ return {
   with_priority({ trig = "atan", name = "atan" }, "\\arctan"),
   with_priority({ trig = "asec", name = "asec" }, "\\arcsec"),
 
-  parse_snippet({ trig = "set", name = "set" }, "\\{$1\\\\}$0"),
+  parse_snippet({ trig = "set", name = "set" }, "\\set{$1}$0"),
+  parse_snippet({ trig = "vec", name = "vector" }, "\\vect{$1}$0"),
   parse_snippet({ trig = "fun", name = "function map" }, "f \\colon $1 \\R \\to \\R \\colon $0"),
 
   parse_snippet(
-    { trig = "abs", name = "absolute value \\abs{}" },
-    "|${1:${TM_SELECTED_TEXT}}|$0"
+    { trig = "abs", name = "absolute value" },
+    "\\abs{$1}$0"
   ),
 }
