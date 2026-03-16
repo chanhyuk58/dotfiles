75c7f408f1ad8fb5a42d202751f5ac1e2378a75e
Chanhyuk Park <chan.hyuk.58@gmail.com>
2026-03-16 13:13:54 -0500

    2026-03-16 13:13 Edits regarding rmarkdown.

--- nvim/ftplugin/rmd.lua
+++ nvim/ftplugin/rmd.lua
@@ -13,3 +13,18 @@ vim.keymap.set("n", ",;", "<Plug>SlimeParagraphSend", { buffer = true, remap = t
 -- vim.o.softtabstop=2
 -- vim.o.shiftwidth=2
 -- vim.o.tabstop=2
+--
+-- Modification dates
+vim.api.nvim_create_autocmd({ "BufWritePre", "FileWritePre" }, {
+  buffer = bufnr,
+  callback = function()
+    local last_line = math.min(20, vim.fn.line("$"))
+    for i = 1, last_line do
+      local line = vim.fn.getline(i)
+      if line:match("^modified:") then
+        vim.fn.setline(i, "modified: " .. os.date("%Y-%m-%d %H:%M:%S"))
+        break
+      end
+    end
+  end,
+})
