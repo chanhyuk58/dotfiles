49c44fb6ef03fbcd91dc7bfc4ea06f16d8654d24
Chanhyuk Park <chan.hyuk.58@gmail.com>
2026-03-19 09:38:57 -0500

    2026-03-19 09:38

--- nvim/lua/custom_snips/lua_snippets/r.lua
+++ nvim/lua/custom_snips/lua_snippets/r.lua
@@ -7,5 +7,5 @@ local auto_lsp_snip = ls.extend_decorator.apply(ls.parser.parse_snippet, auto)
 
 ----------------------------
 return{
-  auto_lsp_snip({trig="lib", name="load library"}, "library(${1:package})")
+  -- auto_lsp_snip({trig="lib", name="load library"}, "library(${1:package})")
 }
